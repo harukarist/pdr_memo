22b7dfd0e989f764e0c40db0a76117bfdd0790db
harukarist <harukarist@gmail.com>
2020-11-17 00:45:30 +0900

    カラムの最大長を変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Heroku環境でhttpsを強制する
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        // カラムの最大長を変更
+        // MySQL5.7.7未満ではユニーク制約を付けたカラムは最大767bytesのため
+        // varchar(191) * 4bytes(utf8mb4) = 764bytes となるように変更。
+        Schema::defaultStringLength(191);
     }
 }
