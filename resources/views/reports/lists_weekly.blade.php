fdc9d9cac5a6aea046950d6c72f0b0af1f510ef1
harukarist <harukarist@gmail.com>
2020-12-13 22:36:12 +0900

    heroku upload

--- resources/views/reports/lists_weekly.blade.php
+++ resources/views/reports/lists_weekly.blade.php
@@ -5,36 +5,31 @@
     </a>
   </section>
   <section class="mb-5">
-    @foreach($lists as $date => $reviews)
-      @php
-        // dd($lists)
-      @endphp
-      @if(count($reviews))
-      {{-- タスクリスト --}}
-      <div class="p-record__wrapper mb-4">
-        <div class="p-record__date border-bottom mb-4 px-2 d-flex">
-          <h5>{{ $date ?? '' }}</h5>
-        </div>
-        <div class="p-record__contents">
-          @component('reports.item')
-          @slot('reviews',$reviews)
-          @endcomponent
-        </div>
-      </div>
-      @else
-        {{-- @if($loop->last)
+    @if(count($lists))
+      @forelse($lists as $date => $reviews)
+        @php
+          // dd($lists)
+        @endphp
+        @if(count($reviews))
+        {{-- タスクリスト --}}
         <div class="p-record__wrapper mb-4">
           <div class="p-record__date border-bottom mb-4 px-2 d-flex">
-            <h5>{{ $startDay.' - '. $lastDay }}</h5>
+            <h5>{{ $date ?? '' }}</h5>
           </div>
           <div class="p-record__contents">
-            <div class="p-report__reviews px-4 py-2 mb-3">
-            記録はまだありません
-            </div>
+            @component('reports.item')
+            @slot('reviews',$reviews)
+            @endcomponent
           </div>
         </div>
-        @endif --}}
+        @endif
+      @empty
+        <div class="p-record__contents">
+          <div class="p-report__reviews px-4 py-2 mb-3">
+          記録はまだありません
+          </div>
+        </div>
+      @endforelse
     @endif
-  @endforeach
   </section>
 </div>
