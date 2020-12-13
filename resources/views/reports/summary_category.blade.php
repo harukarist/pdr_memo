aeb35505ef65ab0e90d8d4dfd5eb4a0c427ad4cf
harukarist <harukarist@gmail.com>
2020-12-13 21:32:12 +0900

    レイアウトを修正

--- resources/views/reports/summary_category.blade.php
+++ resources/views/reports/summary_category.blade.php
@@ -1,12 +1,18 @@
 @if($summaries)
-<div class="p-counter__wrapper mx-2 mb-4">
+<div class="p-counter__wrapper bg-white col-5 mx-2 mb-4 p-2">
+  <div class="row mx-1 mb-2">
+    <div class="col-12 p-0">
+      <span class="badge badge-secondary">カテゴリー別 Total</span><br>
+    </div>
+  </div>
   @foreach($summaries as $category_name=>$summary)  
   @if($summary['total_hour'])
-    <div class="row m-0 bg-white">
+    <div class="row m-0">
         <span class="badge badge-light">{{ $category_name }}</span>
     </div>
-    <div class="row m-0 bg-white">
-      <div class="col-auto">
+    <div class="row mx-2 mb-2">
+      <div class="col-8 p-0">
+        <small>
           <i class="fas fa-medal" aria-hidden="true"></i>
           達成度<br>
           <span class="p-counter__number">
@@ -17,14 +23,17 @@
             {{ $summary['total_count'] ?? 0 }}
           </span>
           回
+        </small>
       </div>
-      <div class="col-auto">
+      <div class="col-4 p-0">
+        <small>
           <i class="far fa-check-circle" aria-hidden="true"></i>
           完了<br>
           <span class="p-counter__number">
             {{ $summary['completed_count'] ?? 0 }}
           </span>
           件
+        </small>
       </div>
     </div>
     @endif
--- resources/views/reports/summary_project.blade.php
+++ resources/views/reports/summary_project.blade.php
@@ -1,14 +1,14 @@
 @if($summaries)
   @foreach($summaries as $project_name=>$summary)  
   @if(isset($summary))
-  <div class="p-counter__wrapper mx-2 mb-4">
-    <div class="row m-0 bg-white">
+  <div class="p-counter__wrapper bg-white col-5 mx-2 mb-4 p-2">
+    <div class="row m-0">
       <small>
         {{ $project_name }}
         <span class="p-counter__number">{{ $summary['days_count'] ?? 0 }}</span> 日目
       </small>
     </div>
-    <div class="row m-0 bg-white">
+    <div class="row m-0">
       <span class="p-counter__number small">最初の達成：{{ $summary['started_at'] ?? '' }}</span>
     </div>
 
@@ -16,59 +16,69 @@
       @foreach($summary['categories'] as $category_name => $category_item)
         {{-- カテゴリー別 --}}
         @if($category_item['total_hour'] )
-          <div class="row m-0 bg-white">
-            <div class="col-auto">
+          <div class="row m-0">
+            <div class="col-12 p-0">
               <span class="badge badge-light">{{ $category_name }}</span><br>
             </div>
           </div>
-          <div class="row m-0 bg-white">
-            <div class="col-auto">
-              <i class="fas fa-medal" aria-hidden="true"></i>
-              達成度<br>
-              <span class="p-counter__number">
-              {{ $category_item['total_hour'] }}
-              </span> h /
-              <span class="p-counter__number">
-              {{ $category_item['total_count'] }}
-              </span> 回
-            </div>
-            <div class="col-auto">
-              <i class="far fa-check-circle" aria-hidden="true"></i>
-              完了<br>
-              <span class="p-counter__number">
-              {{ $category_item['completed_count'] }}
-              </span> 件<br>
-            </div>
+          <div class="row mx-2 mb-1">
+              <div class="col-8 p-0">
+                <small>
+                <i class="fas fa-medal" aria-hidden="true"></i>
+                達成度<br>
+                <span class="p-counter__number">
+                {{ $category_item['total_hour'] }}
+                </span> h /
+                <span class="p-counter__number">
+                {{ $category_item['total_count'] }}
+                </span> 回
+                </small>
+              </div>
+              <div class="col-4 p-0">
+                <small>
+                <i class="far fa-check-circle" aria-hidden="true"></i>
+                完了<br>
+                <span class="p-counter__number">
+                {{ $category_item['completed_count'] }}
+                </span> 件<br>
+                </small>
+              </div>
           </div>
         @endif
       @endforeach
     @endif
 
-    <div class="row m-0 bg-white">
-      <div class="col-auto">
-        <span class="badge badge-secondary">Total</span><br>
+    <div class="bg-light mt-2 p-1 align-self-end">
+      <div class="row m-0">
+        <div class="col-12 p-0">
+          <span class="badge badge-secondary">Total</span><br>
+        </div>
       </div>
-    </div>
-    <div class="row m-0 bg-white">
-      <div class="col-auto">
-          <i class="fas fa-medal" aria-hidden="true"></i>
-          達成度<br>
-          <span class="p-counter__number">
-            {{ $summary['total_hour'] ?? 0 }}
-          </span>
-          h /
-          <span class="p-counter__number">
-            {{ $summary['total_count'] ?? 0 }}
-          </span>
-          回
-      </div>
-      <div class="col-auto">
-          <i class="far fa-check-circle" aria-hidden="true"></i>
-          完了<br>
-          <span class="p-counter__number">
-            {{ $summary['completed_count'] ?? 0 }}
-          </span>
-          件
+      <div class="row m-0">
+        <div class="col-8 p-0">
+          <small>
+            <i class="fas fa-medal" aria-hidden="true"></i>
+            達成度<br>
+            <span class="p-counter__number">
+              {{ $summary['total_hour'] ?? 0 }}
+            </span>
+            h /
+            <span class="p-counter__number">
+              {{ $summary['total_count'] ?? 0 }}
+            </span>
+            回
+          </small>
+        </div>
+        <div class="col-4 p-0">
+          <small>
+            <i class="far fa-check-circle" aria-hidden="true"></i>
+            完了<br>
+            <span class="p-counter__number">
+              {{ $summary['completed_count'] ?? 0 }}
+            </span>
+            件
+          </small>
+        </div>
       </div>
     </div>
   </div>
