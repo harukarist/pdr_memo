92f92b28497a38e4d5c052982d72c98b15478bce
harukarist <harukarist@gmail.com>
2020-12-27 10:38:29 +0900

    fontsを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,11 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <script src="https://kit.fontawesome.com/f2d7c28546.js" crossorigin="anonymous"></script>
+    
+    {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
